c0510ace07ed7c80b3737c703228ecc2e788ca84
HelenaAgustsson <helenafa@stud.ntnu.no>
2021-11-07 16:03:04 +0000

    sending message to server and back to client

--- server.js
+++ server.js
@@ -11,8 +11,19 @@ const io = socketio(server);
 app.use(express.static(path.join(__dirname, 'public')));
 
 //run when a user connects
-io.on('connection', (socket)=>{
-    console.log('New ws connection...');
+io.on('connection', socket =>{
+    //socket.emit -> to the client
+    socket.emit('message','Welcome to chat!')
+    // socket.broadcast.emit -> everyone except the client
+    socket.broadcast.emit('message', 'A user has joined the chat');
+    // io.emit -> everybody
+    socket.on('disconnect', ()=> {
+        io.emit('message', 'A user has left the chat');
+    })
+    //listen for chatmessage
+    socket.on('chatMessage', msg=>{
+        io.emit('message', msg)
+    })
 })
 
 const PORT = process.env.PORT || 3000;
