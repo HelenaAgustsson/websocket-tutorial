6532220eff84f279ee536569b368edf4d632890a
HelenaAgustsson <helenafa@stud.ntnu.no>
2021-11-07 13:39:57 +0000

    set up websocket on server

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const path=require('path');
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io')
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//run when a user connects
+io.on('connection', (socket)=>{
+    console.log('New ws connection...');
+})
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=> console.log('server running'));
+server.listen(PORT, ()=> console.log('server running'));
